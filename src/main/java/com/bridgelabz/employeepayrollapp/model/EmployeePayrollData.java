983fd6c59d04ea931f39f63266bdd313c11db0c7
KarthikBenki <karthikmc007@gmail.com>
2022-05-10 12:15:54 +0530

    Ability to save Employee Payroll Data to MySql DB

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -10,24 +10,34 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
-
+@Entity
+@Table(name = "employee_payroll")
 @Data
 public class EmployeePayrollData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "employee_id")
     private int employeeId;
+    @Column(name = "name")
     private String name;
     private long salary;
     private String gender;
+    @Column(name = "start_date")
     private LocalDate startDate;
     private String note;
+    @Column(name = "profile_pic")
     private String profilePic;
+    @ElementCollection
+    @CollectionTable(name = "employee_department",
+                     joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "department")
     private List<String> departments;
 
 
     public EmployeePayrollData(){}
 
-    public EmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO){
-        this.employeeId=empId;
+    public EmployeePayrollData( EmployeePayrollDTO employeePayrollDTO){
        this.updateEmployeePayrollDTO(employeePayrollDTO);
     }
 
--- src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollserviceImpl.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollserviceImpl.java
@@ -3,13 +3,20 @@ package com.bridgelabz.employeepayrollapp.services;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.exceptions.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import com.bridgelabz.employeepayrollapp.repository.EmployeePayrollRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class EmployeePayrollserviceImpl implements IEmployeePayrollService {
+
+    @Autowired
+    private EmployeePayrollRepository employeePayrollRepository;
 
     private List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
     /**
@@ -33,9 +40,10 @@ public class EmployeePayrollserviceImpl implements IEmployeePayrollService {
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         EmployeePayrollData employeePayrollData = null;
         //employeePayrollDataList.size()+1 because size will be 0 initially
-        employeePayrollData = new EmployeePayrollData(employeePayrollDataList.size()+1, employeePayrollDTO);
+        employeePayrollData = new EmployeePayrollData(employeePayrollDTO);
         employeePayrollDataList.add(employeePayrollData);//adds in employee list
-        return employeePayrollData;
+        log.debug("Emp Data: "+employeePayrollData.toString());
+        return employeePayrollRepository.save(employeePayrollData);
     }
 
     @Override
